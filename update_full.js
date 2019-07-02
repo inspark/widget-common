dde864615025aafb9c5b8704da4792f6b04a2c19
Oleg Mikhailenko <blaremc@gmail.com>
2019-07-02 18:40:01 +0300

    Updated build system

--- update_full.js
+++ update_full.js
@@ -3,7 +3,6 @@ var contents = fs.readFileSync("./dist/widget-common/full/package.json");
 // Define to JSON type
 var jsonContent = JSON.parse(contents);
 
-console.log(jsonContent);
 
 const fields = ['main', 'module', 'es2015', 'esm5', 'esm2015', 'fesm5', 'fesm2015', 'typings', 'metadata'];
 
@@ -12,6 +11,6 @@ fields.forEach(field => {
   jsonContent[field] = 'full/' + jsonContent[field];
 });
 
-console.log(jsonContent);
 
 fs.writeFileSync("./dist/widget-common/package.json", JSON.stringify(jsonContent));
+console.log('Done!');
